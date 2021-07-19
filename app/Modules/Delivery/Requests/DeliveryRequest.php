99b46f33097cbb9c5bdabbc14c5ba64898518948
Artem Chepak <a.chepak91@gmail.com>
2021-07-19 12:37:55 +0300

    add basic functionality

---
+++ app/Modules/Delivery/Requests/DeliveryRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Modules\Delivery\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeliveryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:4|max:50',
+            'city' => 'required',
+            'area' => 'required',
+        ];
+    }
+}
