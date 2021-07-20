e686a8c28d3e47b64f150de63bf7439cffa2276e
Artem Chepak <a.chepak91@gmail.com>
2021-07-20 12:00:11 +0300

    fix: change route base path

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-require_once '../app/Modules/Delivery/Routes/web.php';
